3e1b85b466d13a200e00d076a129a5d61f311bad
Sacha Clael <sachaclael@hotmail.com>
2025-08-20 21:01:39 -0300

    Update oftalmo - 2025-04.py

--- oftalmo - 2025-04.py
+++ oftalmo - 2025-04.py
@@ -1,5 +1,3 @@
-Python 3.13.5 (v3.13.5:6cb20a219a8, Jun 11 2025, 12:23:45) [Clang 16.0.0 (clang-1600.0.26.6)] on darwin
-Enter "help" below or click "Help" above for more information.
 >>> from pysus.online_data.SIH import download
 ... import pandas as pd
 ... from datetime import datetime
